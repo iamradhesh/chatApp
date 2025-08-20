c51a6b6c0653f1e00c1a973f0196c9ef47dfb7a1
Radhesh Pandey <radhesh185@gmail.com>
2025-08-20 14:33:05 +0530

    initialize backend

---
+++ Backend/user/src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || "", {
+      dbName: "chatAppMicroservices"
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+}
---
+++ Backend/user/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
