b1d627fbe3385032acc7620b316096dc88958353
Radhesh Pandey <radhesh185@gmail.com>
2025-08-25 15:19:50 +0530

    otp template changes

--- Backend/mail/src/index.ts
+++ Backend/mail/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { startSendOTPConsumer } from './consumer.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-
+startSendOTPConsumer();
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`🚀 Mail service is running on port ${process.env.PORT || 3000}`);
