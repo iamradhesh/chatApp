c6e59132a2276d872f4d8d4367313d6aefd0ecb9
Radhesh Pandey <radhesh185@gmail.com>
2025-09-11 14:55:28 +0530

    added cors support

--- Backend/chat/src/index.ts
+++ Backend/chat/src/index.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import chatRoutes from './routes/ChatRoutes.js';
-
+import cors from 'cors';
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 const PORT = process.env.PORT || 5003;
 app.use('/api/v1', chatRoutes);
